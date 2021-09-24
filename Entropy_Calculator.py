d4692cb29fb37c827c2dfdf81b2e2f700b22d02d
itay migdal <32266629+itaymigdal@users.noreply.github.com>
2021-09-24 21:40:49 +0300

    Add files via upload

---
+++ Entropy_Calculator.py
@@ -0,0 +1,80 @@
+from math import log
+from sys import argv
+from os import path
+try:
+	import pefile
+	use_pe_parsing = True
+except ModuleNotFoundError:
+	print("[*] Module 'pefile' isn't installed. calculating simple entropy.")
+	use_pe_parsing = False
+
+
+def print_help():
+	print("\n[-] Usage: {} <file>\n".format(argv[0]))
+	exit(1)
+
+
+def calc_shannon_entropy(byte_array):
+
+	byte_array_length = len(byte_array)
+	byte_counts = []  # list of counters for each byte [0-255] in byte array
+	entropy = 0.0     
+
+	# for each byte in [0-255] calculate apperances count
+	for b in range(256):
+		byte_counter = 0
+		for byte in byte_array:
+			if byte == b:
+				byte_counter += 1
+		byte_counts.append(float(byte_counter) / byte_array_length)
+
+	# calculate shannon entropy
+	for byte_count in byte_counts:
+		if byte_count > 0:
+			entropy -= byte_count * log(byte_count, 2)
+	
+	return entropy
+
+
+def pe_parsing(pe_file):
+
+	print("=====================")
+	for section in pe_file.sections:
+		byte_array = list(section.get_data())
+		entropy = calc_shannon_entropy(byte_array)
+		print("[+]", section.Name.decode().ljust(10), "{:.3f}".format(entropy))
+	print("=====================")
+
+
+def basic_parsing(file):
+	with open(file, 'rb') as f:
+		byte_array = list(f.read())
+		entropy = calc_shannon_entropy(byte_array)
+		print("=====================")
+		print("[+] Entropy:   {:.3f}".format(entropy))
+		print("=====================")
+
+
+def main():
+
+	global use_pe_parsing
+
+	if len(argv) != 2 or not (path.isfile(argv[1])):
+		print_help()
+
+	if use_pe_parsing:
+		try:
+			pe_file =  pefile.PE(argv[1])
+			print("[*] File is portable executable. calculating entropy for each section.")
+
+		except pefile.PEFormatError:
+			print("[*] File isn't portable executable. calculating simple entropy.")
+			use_pe_parsing = False
+
+	if use_pe_parsing:
+		pe_parsing(pe_file)
+	else:
+		basic_parsing(argv[1])
+
+
+main()
