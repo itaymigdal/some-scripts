7d536da093fefaaa4ee7a0cfba6d5a4569a0c2af
Itay Migdal <32266629+itaymigdal@users.noreply.github.com>
2022-06-22 14:45:56 +0300

    Add files via upload

--- Privesc_Unquoted_Service.py
+++ Privesc_Unquoted_Service.py
@@ -26,17 +26,11 @@ def print_unquoted_service(unqouted_service):
     print("[!] Unquoted service Found!")
     print(f"\t--> {unqouted_service}")
     print("\tPotential paths to implant bad binary:")
-    binary_path_parts = unqouted_service.split(".exe")[0].split(" ")[:-1]
-    binary_paths = [binary_path_parts[0]]
-    for i in (0, len(binary_path_parts) - 1):
-        binary_paths.append(binary_paths[-1] + binary_path_parts[i])
-    # append .exe for each path
-    binary_paths = [(i + ".exe") for i in binary_paths]
-    for binary_path in binary_paths:
-        if os.access(os.path.dirname(binary_path), os.W_OK):
-            print(f"\t\t{binary_path} <-- Have write access ;-)")
-        else:
-            print(f"\t\t{binary_path} <-- Don't Have write access :-(")
+    binary_path = unqouted_service.split(" ")[0] + ".exe"
+    if os.access(os.path.dirname(binary_path), os.W_OK):
+        print(f"\t\t{binary_path} <-- Have write access ;-)")
+    else:
+        print(f"\t\t{binary_path} <-- Don't Have write access :-(")
 
 
 def main():
